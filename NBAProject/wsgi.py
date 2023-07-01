18f18a241d618b46cb2bb43fafb39b3a443e8d49
jesvazarg <jesvazarg@gmail.com>
2023-07-01 20:40:07 +0200

    Update wsgi.py

--- NBAProject/wsgi.py
+++ NBAProject/wsgi.py
@@ -7,9 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/ubuntu/NBAWebPredictor/NBAProject')
+
+sys.path.append('/home/ubuntu/.local/lib/python3.10/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NBAProject.settings')
 
